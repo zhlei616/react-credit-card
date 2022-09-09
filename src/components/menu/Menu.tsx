888761f9b474e9b19adbb6fa04f4f52fb6cc5e3d
zhlei616 <zhlei616@gmail.com>
2022-09-10 00:21:12 +1200

    Added menu icons;

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import "./Menu.css";
+import menu from "../../asserts/images/menu.png";
+import back from "../../asserts/images/back.png";
 
 function Menu() {
-  const [isOpen, setMenuState] = useState(false);
-  const slideMenu = () => setMenuState(!isOpen);
+  const [isMenuOpen, setMenuState] = useState(false);
+  const slideMenu = () => setMenuState(!isMenuOpen);
 
   return (
     <div>
         <div>
-          <div onClick={slideMenu}>Menu</div>
+          <img className="icon" src={menu} alt="Menu" onClick={slideMenu}/>
         </div>
-        <div className={isOpen ? 'menu open' : 'menu'}>
-          <div onClick={slideMenu}>Back</div>
+        <div className={isMenuOpen ? 'menu open' : 'menu'}>
+          <img className="icon" src={back} alt="Back" onClick={slideMenu}/>
           <hr/>
           <div>This is menu content</div>
         </div>
